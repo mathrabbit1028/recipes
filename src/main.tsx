976add8a6092bf5ee73868851a6df57ae7838ea3
Yeonu-Kim <ywk0524@snu.ac.kr>
2025-06-02 20:30:12 +0900

    🔧 vscode setting 중 biome 켜는 설정 추가

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 
 const rootElement = document.getElementById('root');
-if (!rootElement) {
+if (rootElement === null) {
   throw new Error('Root element not found');
 }
 
